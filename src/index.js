25c1732675c1b3feb368f205ce40a653d02527eb
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-04-29 15:22:09 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList className="skill-list" />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Avatar() {
+  return <img className="avatar" src="cat.jpeg" alt="Adam Bethlehem" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Adam Bethlehem</h1>
+      <p>
+        I am Adam, A beginner fullstack automation engineer. I am now studying
+        to become a full stack we front end engineer. Come on a journey with me!
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <span className="skill-list">
+      <Skill name="JavaScript 🏋️‍♀️" backgroundColor="blue" />
+      <Skill name="Docker 🐳" backgroundColor="pink" />
+      <Skill name="CSS + HTML 🐝" backgroundColor="orange" />
+      <Skill name="Git + Github 🐝" backgroundColor="red" />
+      <Skill name="React 🕸️" backgroundColor="lightblue" />
+    </span>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.backgroundColor }}>
+      {props.name}
+    </div>
+  );
+}
